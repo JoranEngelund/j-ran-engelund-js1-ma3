583416c9257470c4ffbc4fdd216bdf545504b4fe
JoranEngelund <joranengelund@hotmail.com>
2022-04-22 16:59:50 +0200

    Updated js, html and css

---
+++ js/question-1.js
@@ -0,0 +1,7 @@
+// Question 1
+
+getRemainder = (a, b) => a % b;
+
+// to test it works
+const result = getRemainder(10, 5);
+console.log(result);
---
+++ js/question-2.js
@@ -0,0 +1,37 @@
+// Question 2
+
+const loader = document.querySelector(".loader");
+
+function stopLoader() {
+  loader.classList.remove("loader");
+}
+
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=0b59517753684cbb8bcc637960af709a";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getApiRawg() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    stopLoader();
+    const games = data.results;
+
+    for (let i = 0; i < games.length; i++) {
+      if (i === 8) {
+        break;
+      }
+
+      resultsContainer.innerHTML += `<div class="result">
+                                        <div>Name: ${games[i].name}</div>
+                                        <div>Rating: ${games[i].rating}
+                                        <div>Tags: ${games[i].tags.length}</div>
+                                    </div>`;
+    }
+  } catch (error) {
+    resultsContainer.innerHTML += `<div class="result">An error has occurred: ${error} </div>`;
+  }
+}
+
+getApiRawg();
